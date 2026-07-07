056e4adcb47aba89da34cf2068ef2377466db6ca
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-07-07 13:17:28 +0530

    feat: import AttendanceModule and AuthModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     PrismaModule,
+    AttendanceModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
